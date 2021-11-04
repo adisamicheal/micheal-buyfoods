e119d03ccd72582f91a8a7d728c8961811712bf3
adisamicheal <adisamicheal20@gmail.com>
2021-11-04 11:57:56 +0100

    add test

--- tests/unit/accordion.spec.js
+++ tests/unit/accordion.spec.js
@@ -8,13 +8,3 @@ test('Hello', () => {
     expect(wrapper.find('.show').exists()).toBe(false)
 
 })
-// describe('Input.vue', () => {
-//   it('set show to false by default', () => {
-//     const label = 'Bank Verification Number (11-digits)'
-//     const wrapper = shallowMount(Accordion, {
-//       propsData: { label }
-//     })
-//     // assert the error is rendered
-//   expect(wrapper.find('.show').exists()).toBe(false)
-//   })
-// })
